9fc0c8f570bb3895f028639a43e90a7a6cee2a06
Andy Lam <andycylam1@gmail.com>
2026-08-17 00:31:15 +0800

    ci: configure coverage provider and vitest config; lockfile

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: 'node',
-    include: ['tests/**/*.test.ts']
-  }
+    globals: false,
+    environment: 'jsdom',
+    include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx', 'tests/ui/**/*.test.tsx']
+  },
+  coverage: {
+    provider: 'c8',
+    reporter: ['text', 'lcov'],
+    all: true,
+  },
 });
